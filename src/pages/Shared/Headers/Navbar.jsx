46ae5564fa56f6206c2f35b290d3e4346ecf591c
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-06-07 13:19:41 +0600

    navbar added

--- src/pages/Shared/Headers/Navbar.jsx
+++ src/pages/Shared/Headers/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import useTheme from "../../../hooks/useTheme";
 import { BsMoonStarsFill, BsFillBrightnessHighFill } from "react-icons/bs";
+import { NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
@@ -10,12 +11,16 @@ const Navbar = () => {
       {/* logo */}
       <div className="flex justify-center items-center gap-2">
         <img className="w-10 md:w-16" src="https://i.ibb.co/TtWvLPx/mastery-karate-logo.png" alt="" />
-        <h1 className="text-4xl font-kanit font-extrabold">Mastery<span className='text-primary'>Karate</span></h1>
+        <h1 className="text-3xl font-kanit font-extrabold">Mastery<span className='text-primary'>Karate</span></h1>
       </div>
       {/* nav link */}
-      <ul>
-        <li>Home</li>
-      </ul>
+      <div className="flex items-center md:gap-8">
+        <NavLink to="/" className={({isActive})=> isActive ? 'text-primary font-kanit font-medium' : 'text-white font-kanit font-medium'}>Home</NavLink>
+        <NavLink to="/classes" className={({isActive})=> isActive ? 'text-primary font-kanit font-medium' : 'text-white font-kanit font-medium'}>Classes</NavLink>
+        <NavLink to="/instructors" className={({isActive})=> isActive ? 'text-primary font-kanit font-medium' : 'text-white font-kanit font-medium'}>Instructors</NavLink>
+        <NavLink to="/about" className={({isActive})=> isActive ? 'text-primary font-kanit font-medium' : 'text-white font-kanit font-medium'}>About</NavLink>
+        <NavLink to="/contract" className={({isActive})=> isActive ? 'text-primary font-kanit font-medium' : 'text-white font-kanit font-medium'}>Contract</NavLink>
+      </div>
       {/* user/login */}
       <div className="flex justify-center items-center gap-4">
         <button className="my-btn">Login</button>

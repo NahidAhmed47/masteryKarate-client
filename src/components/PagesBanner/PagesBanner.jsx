d7a61408c2c11b35b58b5c97155565244f969ca9
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-06-08 20:24:46 +0600

    classes pages added

---
+++ src/components/PagesBanner/PagesBanner.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+import useTheme from "../../hooks/useTheme";
+
+const PagesBanner = ({ img, title }) => {
+    const {isDarkMode} = useTheme()
+    const location = useLocation();
+  return (
+    <div className={`h-[45vh] w-full ${isDarkMode ? 'bg-gray-500' : 'bg-gray-300'} pt-14 md:pt-40`}>
+      <div className="max-container relative w-full h-full">
+        <img className="absolute bottom-0 -right-10" src={img} alt="" />
+        <div className="pl-5 md:pl-10">
+          <h1 className={`text-3xl font-kanit font-extrabold ${isDarkMode ? 'text-gray-100' : 'text-black'}`}>{title}</h1>
+          <p className={`font-bold text-lg font-kanit mt-3 ${isDarkMode ? 'text-gray-300' : 'text-black'}`}>Home {location.pathname}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PagesBanner;
--- src/pages/DefaultPages/Classes/Classes.jsx
+++ src/pages/DefaultPages/Classes/Classes.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import PagesBanner from "../../../components/PagesBanner/PagesBanner";
+import SectionHeader from "../../../components/SectionHeader/SectionHeader";
+import useClasses from "../../../hooks/useClasses";
+import ClassCard from "../../../components/ClassCard/ClassCard";
 
 const Classes = () => {
-    return (
-        <div>
-            this is class page
+  const [classes] = useClasses();
+  return (
+    <div className={`pt-14 md:pt-40`}>
+      <PagesBanner
+        img="https://i.ibb.co/mTZ3j0G/classes-Page-removebg-preview.png"
+        title="All Mastery Classes"
+      ></PagesBanner>
+      <div className="max-container my-10 md:my-16">
+        <SectionHeader
+          title="Select Class & enjoy"
+          subTitle="Classes"
+        ></SectionHeader>
+        <div className="grid md:grid-cols-2 gap-5 md:gap-8 mt-8">
+          {classes?.map((eachClass) => <ClassCard key={eachClass._id} classes={eachClass}></ClassCard>)}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Classes;

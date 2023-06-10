3471b146766e70ce8a4259d73b15ac67023047bd
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-06-10 12:36:02 +0600

    user can't select class before login done

--- src/pages/DefaultPages/Classes/Classes.jsx
+++ src/pages/DefaultPages/Classes/Classes.jsx
@@ -3,13 +3,40 @@ import PagesBanner from "../../../components/PagesBanner/PagesBanner";
 import SectionHeader from "../../../components/SectionHeader/SectionHeader";
 import ClassCard from "../../../components/ClassCard/ClassCard";
 import useApprovedClasses from "../../../hooks/useApprovedClasses";
+import useInstructors from "../../../hooks/useInstructors";
+import useAuth from "../../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Classes = () => {
   const [approvedClasses] = useApprovedClasses();
+  const [instructors] = useInstructors();
+  const {user, loading} = useAuth();
+  const navigate = useNavigate()
+  const selectClass = (selectedClass)=>{
+    if(!user){
+      Swal.fire({
+        title: 'You must have with loggin',
+        icon: 'info',
+        showCloseButton: true,
+        showCancelButton: true,
+        focusConfirm: false,
+        confirmButtonText:'Login Now',
+        cancelButtonText:
+          'Later',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          navigate('/login', {replace: true});
+        }
+      })
+      return
+    }
+    const {instructor_email, _id, available_seats } = selectedClass;
+    console.log(instructor_email, available_seats, _id)
+    const instructor = instructors?.find(instructor => instructor.email === instructor_email);
+    console.log(instructor)
+  }
 //   TODO: Select Button. If the user is not logged in, then tell the user to log in before selecting the course. This button will be disabled if:
-// Available seats are 0 ...also do it homepage popular classes
-// Logged in as admin/instructor
-// The class card background will be red if the available seats are 0.
   return (
     <div className={`pt-14 md:pt-40`}>
       <PagesBanner
@@ -18,11 +45,11 @@ const Classes = () => {
       ></PagesBanner>
       <div className="max-container my-10 md:my-16">
         <SectionHeader
-          title="Select Class & enjoy"
+          title="Select Class & Gain Skills"
           subTitle="Classes"
         ></SectionHeader>
         <div className="grid md:grid-cols-2 gap-5 md:gap-8 mt-8">
-          {approvedClasses?.map((eachClass) => <ClassCard key={eachClass._id} classes={eachClass}></ClassCard>)}
+          {approvedClasses?.map((eachClass) => <ClassCard key={eachClass._id} classes={eachClass} selectClass={selectClass}></ClassCard>)}
         </div>
       </div>
     </div>

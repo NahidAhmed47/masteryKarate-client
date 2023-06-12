a81f9f9c22a8ef06b3b28271c932e752cd922e57
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-06-12 20:54:10 +0600

    popular classes sort by students

--- src/pages/DefaultPages/Home/PopularClasses/PopularClasses.jsx
+++ src/pages/DefaultPages/Home/PopularClasses/PopularClasses.jsx
@@ -1,27 +1,18 @@
-import React  from 'react';
+import React, { useEffect, useState }  from 'react';
 import SectionHeader from '../../../../components/SectionHeader/SectionHeader';
-import ClassCard from '../../../../components/ClassCard/ClassCard';
-import useApprovedClasses from '../../../../hooks/useApprovedClasses';
 import CardContainer from '../../../../components/CardContainer/CardContainer';
+import axios from 'axios';
 
 const PopularClasses = () => {
-  const [approvedClasses] = useApprovedClasses();
-  const separateByDescending = (data, property)=>{
-    return [...data].reduce((acc, currentObj) => {
-        const index = acc.findIndex((item) => currentObj[property] >= item[property]);
-        if (index === -1) {
-          acc.push(currentObj);
-        } else {
-          acc.splice(index, 0, currentObj);
-        }
-        return acc;
-      }, []);
-  }
-  const separatedClasses = separateByDescending(approvedClasses, "number_of_students").slice(0,6);
+  const [popularClasses , setPopularClasses] = useState([]);
+  useEffect(()=>{
+    axios.get('http://localhost:5000/popular-classes')
+    .then(res => setPopularClasses(res.data))
+  },[])
   return (
     <div className='my-14 md:my-24 max-container'>
         <SectionHeader title="Popular Classes" subTitle="Best Selling"></SectionHeader>
-        <CardContainer classesData={separatedClasses}></CardContainer>
+        <CardContainer classesData={popularClasses}></CardContainer>
     </div>
   );
 };

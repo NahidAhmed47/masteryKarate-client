539a842587c8d696ad7d9d0487673ae4c6c9b65d
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-08-06 14:20:02 +0600

    contact page dark mode implement done

--- src/pages/DefaultPages/Contract/Contract.jsx
+++ src/pages/DefaultPages/Contract/Contract.jsx
@@ -3,36 +3,38 @@ import setTitle from '../../../utls/setTitle';
 import PagesBanner from '../../../components/PagesBanner/PagesBanner';
 import { BsFillEnvelopeFill, BsFillGeoAltFill, BsTelephoneFill } from 'react-icons/bs';
 import Subscribe from '../../../components/Subscribe/Subscribe';
+import useTheme from '../../../hooks/useTheme';
 
 const Contract = () => {
+    const {isDarkMode} = useTheme();
     setTitle('Contract')
     return (
         <div className='pt-14 md:pt-40'>
             <PagesBanner img="https://i.ibb.co/2SXWwVN/istockphoto-1480024970-612x612-removebg-preview.png" title="Contact Us"></PagesBanner>
             <div className='max-container grid md:grid-cols-3 gap-4 md:gap-7 lg:gap-10 my-12 md:my-16'>
-                <div className='h-[200px] w-full bg-white flex flex-col justify-center items-center group' style={{boxShadow: '0 0 11px 3px rgba(0, 0, 0, 0.05)'}}>
-                    <div className='p-3 border border-dashed border-primary rounded-full bg-white group-hover:bg-primary duration-500'>
+                <div className={`duration-500 h-[200px] w-full rounded-md ${isDarkMode ? 'border border-gray-700' : 'bg-white'} flex flex-col justify-center items-center group`} style={{boxShadow: '0 0 11px 3px rgba(0, 0, 0, 0.05)'}}>
+                    <div className={`p-3 border border-dashed border-primary rounded-full group-hover:bg-primary duration-500 duration-500 ${isDarkMode ? '' : 'bg-white'}`}>
                         <BsFillGeoAltFill className='w-6 h-6 text-primary group-hover:text-white duration-500'></BsFillGeoAltFill>
                     </div>
-                    <h1 className='text-2xl font-bold text-neutral mt-3'>Address</h1>
+                    <h1 className={`text-2xl font-bold duration-500 mt-3 ${isDarkMode ? 'text-gray-200' : 'text-neutral'}`}>Address</h1>
                     <p className='text-gray-400'>32 st Kilda Road, Melbourne VIC, 3004 Australia</p>
                 </div>
-                <div className='h-[200px] w-full bg-white flex flex-col justify-center items-center group' style={{boxShadow: '0 0 11px 3px rgba(0, 0, 0, 0.05)'}}>
-                    <div className='p-3 border border-dashed border-primary rounded-full bg-white group-hover:bg-primary duration-500'>
+                <div className={`duration-500 h-[200px] w-full rounded-md ${isDarkMode ? 'border border-gray-700' : 'bg-white'} flex flex-col justify-center items-center group`} style={{boxShadow: '0 0 11px 3px rgba(0, 0, 0, 0.05)'}}>
+                    <div className={`p-3 border border-dashed border-primary rounded-full group-hover:bg-primary duration-500 ${isDarkMode ? '' : 'bg-white'}`}>
                         <BsFillEnvelopeFill className='w-6 h-6 text-primary group-hover:text-white duration-500'></BsFillEnvelopeFill>
                     </div>
-                    <h1 className='text-2xl font-bold text-neutral mt-3'>Email</h1>
+                    <h1 className={`text-2xl font-bold duration-500 mt-3 ${isDarkMode ? 'text-gray-200' : 'text-neutral'}`}>Email</h1>
                     <p className='text-gray-400'>nahidahmedsd47@gmail.com</p>
                 </div>
-                <div className='h-[200px] w-full bg-white flex flex-col justify-center items-center group' style={{boxShadow: '0 0 11px 3px rgba(0, 0, 0, 0.05)'}}>
-                    <div className='p-3 border border-dashed border-primary rounded-full bg-white group-hover:bg-primary duration-500'>
+                <div className={`duration-500 h-[200px] w-full rounded-md ${isDarkMode ? 'border border-gray-700' : 'bg-white'} flex flex-col justify-center items-center group`} style={{boxShadow: '0 0 11px 3px rgba(0, 0, 0, 0.05)'}}>
+                    <div className={`p-3 border border-dashed border-primary rounded-full group-hover:bg-primary duration-500 duration-500 ${isDarkMode ? '' : 'bg-white'}`}>
                         <BsTelephoneFill className='w-6 h-6 text-primary group-hover:text-white duration-500'></BsTelephoneFill>
                     </div>
-                    <h1 className='text-2xl font-bold text-neutral mt-3'>Phone</h1>
+                    <h1 className={`text-2xl font-bold duration-500 mt-3 ${isDarkMode ? 'text-gray-200' : 'text-neutral'}`}>Phone</h1>
                     <p className='text-gray-400'>(+880)1312397286</p>
                 </div>
             </div>
-            <div className='w-full h-fit bg-[#f6f5fb] p-4 md:p-8 md:py-14 my-32 max-container'>
+            <div className={`w-full h-fit ${isDarkMode ? '' : 'bg-[#f6f5fb]'} p-4 md:p-8 md:py-14 my-32 max-container`}>
                 <div className='max-w-[1320px] mx-auto grid md:grid-cols-2'>
                     <div className='flex items-center justify-center'>
                         <img src="/public/contact.webp" alt='contractImg' ></img>
